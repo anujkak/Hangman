b87ecca6637d22a90e1db7d929439a1646c6d8cf
Anuj Kak <kakanuj@gmail.com>
2016-05-04 15:00:03 -0700

    Dictionary Lists added. Need to add method for inserting words into dictionary.

--- Hangman/src/DictionaryLists.java
+++ Hangman/src/DictionaryLists.java
@@ -1,4 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
 
 public class DictionaryLists {
-
+	
+private String listTopic;
+private String wordListLocation;
+private int listCount;
+private String[] topicArray = {"science", "capitals", "astronomy", "history", "states"};
+	public DictionaryLists(String topic, String path)
+	{
+		listTopic = topic;
+		wordListLocation = path;
+	}
+	public boolean topicChecker()
+	{
+		if (Arrays.asList(topicArray).contains(listTopic)){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
+	public String[] ReadFile() throws IOException{
+		FileReader fileRead = new FileReader(wordListLocation);
+		BufferedReader textRead = new BufferedReader(fileRead);
+		textRead.close();
+		return topicArray;
+		
+		
+	}
+	private int LinesCounter() throws IOException{
+		int lineCount = 0;
+		FileReader fileRead = new FileReader(wordListLocation);
+		BufferedReader textRead = new BufferedReader(fileRead);
+		String line;
+		while ((line = textRead.readLine()) != null){
+			lineCount ++;
+		}
+		textRead.close();
+		return lineCount;
+	}
+	
+	
 }
